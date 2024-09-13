3c85ba4f7bec8dedb3a5a556d7778d60883ce005
Alexey Staburov <gilbertfrost@yandex.ru>
2024-09-13 14:37:06 +0300

    creat token and middleware

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const Datastore = require("nedb");
 const path = require("path");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 3000;
+const SECRET_KEY = "your_secret_key"; // Задайте секретный ключ для подписи JWT
 
 // Подключение к двум базам данных
 const usersDb = new Datastore({
@@ -40,33 +42,54 @@ app.post("/users", (req, res) => {
   });
 });
 
-// Эндпоинт для получения всех пользователей
-app.get("/users", (req, res) => {
-  usersDb.find({}, (err, docs) => {
-    if (err) {
-      return res.status(500).json({ error: err.message });
-    }
-    res.status(200).json(docs);
-  });
-});
-
-// Эндпоинт для поиска пользователя по mail и password
-app.post("/users/search", (req, res) => {
+// Эндпоинт для поиска пользователя по mail и password и получения токена
+app.post("/users/login", (req, res) => {
   const { mail, password } = req.body;
 
-  usersDb.findOne({ mail, password }, (err, doc) => {
+  usersDb.findOne({ mail, password }, (err, user) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
-    if (!doc) {
+    if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json(doc);
+
+    // Генерируем JWT токен с сроком действия 3 часа
+    const token = jwt.sign({ userId: user._id, mail: user.mail }, SECRET_KEY, {
+      expiresIn: "3h", // Токен будет действовать 3 часа
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: user._id,
+        mail: user.mail,
+        role: user.role,
+      },
+    });
   });
 });
 
-// Эндпоинт для добавления резервации
-app.post("/reserve", (req, res) => {
+// Middleware для проверки токена авторизации
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Token is missing" });
+  }
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ message: "Invalid token" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+// Эндпоинт для добавления резервации (требуется авторизация)
+app.post("/reserve", authenticateToken, (req, res) => {
   const { userId, date, time, service } = req.body;
   const reserve = { userId, date, time, service };
 
@@ -78,8 +101,8 @@ app.post("/reserve", (req, res) => {
   });
 });
 
-// Эндпоинт для получения всех резерваций
-app.get("/reserves", (req, res) => {
+// Эндпоинт для получения всех резерваций (требуется авторизация)
+app.get("/reserves", authenticateToken, (req, res) => {
   reservesDb.find({}, (err, docs) => {
     if (err) {
       return res.status(500).json({ error: err.message });
